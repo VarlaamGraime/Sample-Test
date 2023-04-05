4b2c1aacf4d4bc393b97875a3d580a46ef7dd4d3
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-04-05 18:29:15 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
 import Header from './Components/Header.vue'
 import 'vuetify/dist/vuetify.min.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 library.add(faPhone)
 Vue.config.productionTip = false
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -28,3 +32,4 @@ Vue({
   render: h => h(App)
 })
 Vue.use(Vuetify)
+Vue.use(BootstrapVue)
